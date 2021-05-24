259cda8d5e5efc5c3b49367cf9f3bdc09d9ff2d6
ShivanshPro <49151312+ShivanshPro@users.noreply.github.com>
2021-05-24 20:49:02 +0530

    Delete backup_reseller.sh

--- backup_reseller.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-if [ "$#" -eq 0 ] ; then
-        echo "Please specify the reseller username to proceed"
-elif [ "$#" -eq 1 ] ; then
-        arg=$(echo $1)
-        echo $1|grep '@'
-        for user in $(cat /etc/trueuserowners | grep -w $arg | awk -F":" '{print $1}'); do mkdir -p /backup/$arg && /scripts/pkgacct $user /backup/$arg; done;
-fi
